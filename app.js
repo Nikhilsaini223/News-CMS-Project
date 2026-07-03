582e5026861f682ddd1a70f5e8cd8d2408b61087
Nikhilsaini223 <nikhilsaini4115@gmail.com>
2026-07-03 08:45:09 +0530

    NEWS CMS PROJECT

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const path = require('path')
+const expressSession = require('express-session')
+const connectFlash = require('connect-flash')
+
+require('dotenv').config()
+
+//MIDDLEWARES
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(expressLayouts)
+app.set('layout', 'layouts')
+
+//VIEW ENGINE
+app.set('view engine', 'ejs')
+
+//DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URI)
+  
+
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(connectFlash())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
+})
